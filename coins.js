8acb0e88719d58b47b476af19fc002011d198dd1
krieger27 <krieger@vols.utk.edu>
2017-12-19 20:41:42 -0600

    redo

--- coins.js
+++ coins.js
@@ -1,3 +1,19 @@
+let input = document.getElementById("input");
+let button = document.getElementById("button");
+let output = document.getElementById("output");
+
+button.addEventListener("click", coinCounter);
+
+function coinCounter () {
+    var coinPurse = {}; 
+    var inputFix = input.value * 100;
+    coinPurse.quarters = Math.floor(inputFix / 25);
+    coinPurse.dimes = Math.floor((inputFix % 25) / 10);
+    coinPurse.nickels = Math.floor(((inputFix % 25) % 10) / 5);
+    coinPurse.pennies = Math.floor((((inputFix % 25) % 10) % 5) / 1);
+    console.log(coinPurse);
+  }
+
 /*
   Input: 0.67
   Output:
@@ -9,26 +25,23 @@
   }
 
 */
-let input =document.getElementById("input");
-let button=document.getElementById("button");
-let output = document.getElementById("output");
+// let input =document.getElementById("input");
+// let button=document.getElementById("button");
+// let output = document.getElementById("output");
 
-button.removeEventListener("click", coinCounter);
+// button.addEventListener("click", coinCounter);
 
-function coinCounter (input) {
-    // Initialize a JavaScript object to hold the coins
-    console.log(Math.floor(input.value /.25));
-    var coinPurse = {};
+// function coinCounter (input) {
+//     // Initialize a JavaScript object to hold the coins
+//     var coinPurse = {};
     
-    var inputFix =input.value *100;
+//     var inputFix =input.value *100;
 
-    coinPurse.quarters = Math.floor(inputFix.value /.25);
+//     coinPurse.quarters = Math.floor(inputFix.value /25);
     
-    coinPurse.dimes =Math.floor((inputFix.value % .25) / .10);
-    coinPurse.nickels=Math.floor(((inputFix.value % .25) % .10)/.05);
-    coinPurse.pennies=Math.floor((((inputFix.value % .25) %.10) %.05)) *100;
-    return coinPurse;
-  }
-  
-  var coins = coinCounter()
-  console.log();
+//     coinPurse.dimes =Math.floor((inputFix.value % 25) / 10);
+//     coinPurse.nickels=Math.floor(((inputFix.value % 25) % 10)/5);
+//     coinPurse.pennies=Math.floor((((inputFix.value % 25) %10) %05)/1);
+//     console.log (coinPurse);
+//   }
+
